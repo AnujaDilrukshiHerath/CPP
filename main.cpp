795fbc4be9e5368abf8da0e0d1305145aa6d55c7
Anuja Dilrukshi Herath <66720764+AnujaDilrukshiHerath@users.noreply.github.com>
2021-07-15 21:11:23 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	cout << "Enter your name:"  <<flush;
+	string input;
+	cin >>input;
+	
+	cout<< "Enter your age:" <<flush;
+	int value;
+	cin>> value;
+	
+	
+	
+	
+	return 0;
+}
